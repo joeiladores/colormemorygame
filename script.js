98e75f2a1af713afcc962985cebff343d820d94d
joeiladores <joelynladores@gmail.com>
2022-12-04 23:58:30 +0800

    Revised functions, fixed randomizer and array sorting, fixed clear, submit and restart fuctions

--- script.js
+++ script.js
@@ -1,173 +1,184 @@
-let answer = [], winningCombo = []
-var counter = 0
-const colors = ["blue", "red", "green", "yellow", "pink", "violet"];
+const answer = [];
+var winningCombo = [];
+var counter = 0;
 
-
-// TODO: TRY IMPLEMENTING GETTING RANDOM COLORS FROM A STRING OF COLORS ARRAY AND JUST REARRANGE THEM?
-// TODO: TRY ALSO JUST SORTING THE ARRAY IN RANDOM ORDER 
-// DONE
-
-function generateRandomColors() {    
-  colors.sort(function(){return 0.5 - Math.random()});
-  return colors;
+window.onload = function () {
+  hideBoxUI();
+  hideAnswerUI();
+  hideRestartBtn();
+  hideTimer();
 }
 
-function displayBoxColor() {
-
-  let numbers = 6, min = 0;
-  // GET RANDOM COLORS
-  winningCombo = generateRandomColors()
-
-  //change the color of the box
-  let box = document.querySelector("#box-holder");
-  box.style.visibility = "visible";
-
-  // TODO: STUDY ARRAY OF ELEMENTS IF THERE ARE ANY OTHER WAY TO MANIPULATE THE ELEMENTS CHILDREN USING ELEMENTS ARRAY
-
-  for(i = min; i < numbers; i++) {
-    // let color = getColor(winningCombo[i]);
-    box.children[i].style.backgroundColor = winningCombo[i];
-  } 
-}
-
-function hideBoxColor() {
-  document.querySelector("#box-holder").style.visibility = "hidden"     
-}
-
-function hideAnswerBoxColor() {
-  document.querySelector("#box-answer-holder").style.visibility = "hidden"    
-}
-
-function displayAnswerBox() {         
-
-  const answerBoxColor = ["blue", "red", "green", "yellow", "pink", "violet"]
-  
-  const answerBox = document.querySelector("#box-answer-holder")
-  answerBox.style.visibility = "visible"
-  
-  // COLOR THE ANSWER BOXES
-  for(i = 0; i < 6; i++) {
-    answerBox.children[i].style.backgroundColor = answerBoxColor[i]
-  } 
-}
-
-function todoMouseOnClick (event) {      
-
-  let targetElement = event.target
-  
-  // CHECK WHICH ANSWER COLOR BOXES WERE CLICKED/TARGETED
-  if(targetElement.id == "box1a" && targetElement.dataset.answered == "false") {
-    targetElement.dataset.answered = "true"
-    answer.push("blue")
-    targetElement.textContent = counter + 1
-    counter++
-  }
-  else if(targetElement.dataset.color == "red" && targetElement.dataset.answered == "false") {
-    targetElement.dataset.answered = "true"
-    answer.push("red")
-    targetElement.textContent = counter + 1
-    counter++
-  }
-  else if(targetElement.dataset.color == "green" && targetElement.dataset.answered == "false") {
-    targetElement.dataset.answered = "true"
-    answer.push("green")
-    targetElement.textContent = counter + 1
-    counter++
-  }
-  else if(targetElement.dataset.color == "yellow" && targetElement.dataset.answered == "false") {
-    targetElement.dataset.answered = "true"
-    answer.push("yellow")
-    targetElement.textContent = counter + 1
-    counter++
-  }
-  else if(targetElement.dataset.color == "pink" && targetElement.dataset.answered == "false") {
-    targetElement.dataset.answered = "true"
-    answer.push("pink")
-    targetElement.textContent = counter + 1
-    counter++
-  }
-  else if(targetElement.dataset.color == "violet" && targetElement.dataset.answered == "false") {
-    targetElement.dataset.answered = "true"
-    answer.push("violet")
-    targetElement.textContent = counter + 1
-    counter++
-  }     
+function restartGame() {
+  clearAnswer();
+  hideAnswerUI();
+  hideRestartBtn();
+  hideTimer();
+  startGame();
 }
 
 function todoSubmit() {
 
   // DISPLAY AGAIN CURRENT BOX COLORS
-  document.querySelector("#box-holder").style.visibility = "visible"     
+  displayBoxUI(winningCombo);
+  document.querySelector("#box-holder").style.visibility = "visible";
 
   // COMPARE TWO ARRAYS
   if(winningCombo.toString() == answer.toString()) {
-    document.getElementById("modalResultArea").innerHTML = "Congratulations. You win!"
+    document.getElementById("modalResultArea").innerHTML = "Congratulations. You win!";
   }
   else {
-    document.getElementById("modalResultArea").innerHTML = "Sorry. Try again."
+    document.getElementById("modalResultArea").innerHTML = "Sorry. Try again.";
   }
+
+  document.querySelector("#timer").style.display = 'none';
+  document.querySelector("#restartBtn").style.display = 'block'; 
+  hideClearSubmitBtn();
 }
 
 function clearAnswer() {
-  while(answer.length > 0) {
-    answer.pop();
-  }
-
-  // RESET ANSWER BOXES
   let element = Array.from(document.querySelectorAll(".boxAnswer"))
+  
   element.forEach(element => {
-  element.dataset.answered = "false";
-  element.textContent = ""      
+    element.dataset.answered = "false";
+    element.textContent = ""      
   });
-
   counter = 0;
-
-  console.log(winningCombo)
-  console.log(answer); 
-  console.log(counter)
-}
-
-function clearReset() {
 
   while(answer.length > 0) {
     answer.pop();
   }
-  while(winningCombo.length > 0) {
-    winningCombo.pop();
-  }
-  counter = 0;
-
-  // TEST
-  // console.log(winningCombo)
-  // console.log(answer); 
-  // console.log(counter)
+  
 }
 
-function todoStart() {
+function todoMouseOnClick (event) {      
 
-  displayBoxColor();
-  hideAnswerBoxColor();      
+  let targetElement = event.target;
+  
+  // CHECK WHICH ANSWER COLOR BOXES WERE CLICKED/TARGETED
+  if(targetElement.id == "box1a" && targetElement.dataset.answered == "false") {
+    targetElement.dataset.answered = "true";    
+    answer.push(targetElement.dataset.color);
+    targetElement.textContent = ++counter;    
+  }
+  if(targetElement.id == "box2a" && targetElement.dataset.answered == "false") {
+    targetElement.dataset.answered = "true";
+    answer.push(targetElement.dataset.color);
+    targetElement.textContent = ++counter;   
+  }
+  if(targetElement.id == "box3a" && targetElement.dataset.answered == "false") {
+    targetElement.dataset.answered = "true";
+    answer.push(targetElement.dataset.color);
+    targetElement.textContent = ++counter;   
+  }
+  if(targetElement.id == "box4a" && targetElement.dataset.answered == "false") {
+    targetElement.dataset.answered = "true";
+    answer.push(targetElement.dataset.color);
+    targetElement.textContent = ++counter;   
+  }
+  if(targetElement.id == "box5a" && targetElement.dataset.answered == "false") {
+    targetElement.dataset.answered = "true";
+    answer.push(targetElement.dataset.color);
+    targetElement.textContent = ++counter;   
+  }
+  if(targetElement.id == "box6a" && targetElement.dataset.answered == "false") {
+    targetElement.dataset.answered = "true";
+    answer.push(targetElement.dataset.color);
+    targetElement.textContent = ++counter;       
+  }    
+}
 
+function displayAnswerUI(rAnswerColors) {
+  displayClearSubmitBtn();
+  document.querySelector(".answerUI").style.visibility = 'visible';
+  const answerBox = document.querySelector("#box-answer-holder");
+  for(i = 0; i < 6; i++) {
+    answerBox.children[i].dataset.color = rAnswerColors[i];
+    answerBox.children[i].style.backgroundColor = rAnswerColors[i];
+  }   
+}
+
+function hideBoxUI() {
+  document.querySelector("#box-holder").style.visibility = "hidden"     
+}
+
+function hideAnswerUI() {
+  document.querySelector(".answerUI").style.visibility = 'hidden';
+}
+
+function hideRestartBtn() {
+  document.querySelector("#restartBtn").style.display = 'none'; 
+}
+
+function displayRestartBtn() {
+  document.querySelector("#restartBtn").style.display = 'block'; 
+}
+
+function hideTimer() {
+  document.querySelector("#timer").style.display = 'none'; 
+}
+
+function displayTimer() {
+  document.querySelector("#timer").style.display = 'block'; 
+}
+
+function hideClearSubmitBtn() {
+  document.querySelector("#clearBtn").style.visibility = 'hidden';
+  document.querySelector("#submitBtn").style.visibility = 'hidden';
+}
+
+function displayClearSubmitBtn() {
+  document.querySelector("#clearBtn").style.visibility = 'visible';
+  document.querySelector("#submitBtn").style.visibility = 'visible';
+}
+
+function displayBoxUI(rColors) {
+  let box = document.querySelector("#box-holder");
+  box.style.visibility = "visible";
+  for(i = 0; i < rColors.length; i++) {
+    box.children[i].style.backgroundColor = rColors[i];
+  } 
+}
+
+function getRandomColors(rColors, n) {
+  rColors.sort(function(){return 0.5 - Math.random()});
+  return rColors.slice(rColors.length - n);
+}
+
+function startGame() {
+
+  let randomAnswerColors = [];
+  const colors = ["blue", "red", "green", "yellow", "pink", "purple", "orange", "gray", "brown"];
+  let counter1 = 0;
+
+  const temp = getRandomColors(colors, 6);;
+  winningCombo = temp.map(items);
+
+  function items(value) {
+    return value;
+  }
+
+  randomAnswerColors = getRandomColors(temp, 6);
+
+  document.getElementById("startBtn").style.display = "none";
+  document.getElementById("timer").style.display = "block";
+
+  displayBoxUI(winningCombo);
+  
   setTimeout(function(){
     // TODO AFTER FIVE SECONDS
-    hideBoxColor();
-    displayAnswerBox();
+    hideBoxUI();
+    displayTimer();
+    displayAnswerUI(randomAnswerColors);
+    
   }, 5000);
 
 }
 
-function todoReset() {
-// RESET EVERYTHING
 
-// RESET DATA SET
-let element = Array.from(document.querySelectorAll(".boxAnswer"))
-element.forEach(element => {
-  element.dataset.answered = "false";
-  element.textContent = ""      
-});
 
-clearReset()
-hideBoxColor()
-todoStart()
 
-}
+
+
+
+
